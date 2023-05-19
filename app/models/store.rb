ea8bf66919d985ebea8f505fdc77aa9a94421e05
prometheus <berjistechnologies@gmail.com>
2023-05-19 16:50:56 +0300

    Store validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,10 @@
 class Store < ApplicationRecord
   belongs_to :user
+
+  has_many :store_contacts, dependent: :destroy
+  has_many :store_branches, dependent: :destroy
+  has_many :staffs, dependent: :destroy
+  has_many :roles, dependent: :destroy
+
+  validates_presence_of :name, :type, :coordinates, :location_name
 end
