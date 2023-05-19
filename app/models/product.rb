f11499c3e8f9a418865c5167cb6f1343581a3b10
prometheus <berjistechnologies@gmail.com>
2023-05-19 16:52:12 +0300

    Product Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :store_branch
+
+  has_many :product_variants, dependent: :destroy
+  has_many :product_images, dependent: :destroy
+  has_many :product_branch_availabilities, dependent: :destroy
+  has_many :deliveries, dependent: :destroy
+
+  validates_presence_of :name, :description, :price, :product_type, :availability
 end
