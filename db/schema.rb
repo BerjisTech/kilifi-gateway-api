41a23899b1de14484123995ba4fb876eed0a94fb
prometheus <berjistechnologies@gmail.com>
2023-05-19 16:27:58 +0300

    Reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_132637) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_132747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_132637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_owners_on_user_id"
+  end
+
+  create_table "reviews", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "service_id", null: false
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_reviews_on_service_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "service_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -81,6 +92,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_132637) do
   end
 
   add_foreign_key "owners", "users"
+  add_foreign_key "reviews", "services"
+  add_foreign_key "reviews", "users"
   add_foreign_key "services", "service_types"
   add_foreign_key "services", "users"
 end
