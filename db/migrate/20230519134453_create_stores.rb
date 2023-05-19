9f5a8ec63417c3c8346f50484f5f7b3d02caa7af
prometheus <berjistechnologies@gmail.com>
2023-05-19 16:44:59 +0300

    Store scaffold

---
+++ db/migrate/20230519134453_create_stores.rb
@@ -0,0 +1,13 @@
+class CreateStores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stores, id: :uuid do |t|
+      t.string :name
+      t.string :type
+      t.string :coordinates
+      t.string :location_name
+      t.references :user, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
