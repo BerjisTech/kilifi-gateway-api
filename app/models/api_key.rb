6a55e2a3bad26a711cdb464ffabb4e3a1a0ca832
prometheus <berjistechnologies@gmail.com>
2023-05-19 17:07:50 +0300

    ApiKey generation

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,3 +1,16 @@
 class ApiKey < ApplicationRecord
   belongs_to :user
+
+  before_create :generate_access_token
+  before_create :generate_secret_token
+
+  private
+
+  def generate_access_token
+    self.access_token = SecureRandom.hex
+  end
+
+  def generate_secret_token
+    self.secret_token = SecureRandom.hex
+  end
 end
