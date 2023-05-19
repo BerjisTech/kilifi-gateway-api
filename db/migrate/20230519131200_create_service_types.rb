4ac7f961d85a19492c22c4089c107f5ca2b63e8f
prometheus <berjistechnologies@gmail.com>
2023-05-19 16:12:08 +0300

    Service model

---
+++ db/migrate/20230519131200_create_service_types.rb
@@ -0,0 +1,10 @@
+class CreateServiceTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :service_types, id: :uuid do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
